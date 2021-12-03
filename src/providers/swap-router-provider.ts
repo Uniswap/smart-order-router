7aa4832d8ecdda98a1d72c879fd0a5897ab3f35b
Will Pote <will@uniswap.org>
2021-12-03 14:38:35 -0500

    Handle result

--- src/providers/swap-router-provider.ts
+++ src/providers/swap-router-provider.ts
@@ -1,14 +1,14 @@
-import { CurrencyAmount, Currency } from '@uniswap/sdk-core';
+import { Currency, CurrencyAmount } from '@uniswap/sdk-core';
 import { SwapRouter02__factory } from '../types/other';
+import { log } from '../util';
 import { IMulticallProvider } from './multicall-provider';
-import { ProviderConfig } from './provider';
 
 type ApprovalTypes = {
-	approvalTokenIn: number;
-	approvalTokenOut: number;
-}
+  approvalTokenIn: number;
+  approvalTokenOut: number;
+};
 
-const SWAP_ROUTER_ADDRESS = '0x075B36dE1Bd11cb361c5B3B1E80A9ab0e7aa8a60'
+const SWAP_ROUTER_ADDRESS = '0x075B36dE1Bd11cb361c5B3B1E80A9ab0e7aa8a60';
 
 /**
  * Provider for accessing the SwapRouter02 Contract .
@@ -25,41 +25,53 @@ export interface ISwapRouterProvider {
    * @returns A token accessor with methods for accessing the tokens.
    */
   getApprovalType(
-		tokenInAmount: CurrencyAmount<Currency>,
-    tokenOutAmount: CurrencyAmount<Currency>,
+    tokenInAmount: CurrencyAmount<Currency>,
+    tokenOutAmount: CurrencyAmount<Currency>
   ): Promise<ApprovalTypes>;
 }
 
 export class SwapRouterProvider implements ISwapRouterProvider {
-  constructor(
-    protected multicall2Provider: IMulticallProvider
-  ) {}
+  constructor(protected multicall2Provider: IMulticallProvider) {}
 
   public async getApprovalType(
     tokenInAmount: CurrencyAmount<Currency>,
-    tokenOutAmount: CurrencyAmount<Currency>,
+    tokenOutAmount: CurrencyAmount<Currency>
   ): Promise<ApprovalTypes> {
-		const functionParams: [string, string][] = [
-			[tokenInAmount.currency.wrapped.address, tokenInAmount.quotient.toString()],
-			[tokenOutAmount.currency.wrapped.address, tokenOutAmount.quotient.toString()],
-		]
+    const functionParams: [string, string][] = [
+      [
+        tokenInAmount.currency.wrapped.address,
+        tokenInAmount.quotient.toString(),
+      ],
+      [
+        tokenOutAmount.currency.wrapped.address,
+        tokenOutAmount.quotient.toString(),
+      ],
+    ];
 
-		const tx = await this.multicall2Provider.callSameFunctionOnContractWithMultipleParams<
-			[string, string],
-			number
-		>({
-			address: SWAP_ROUTER_ADDRESS,
-			contractInterface: SwapRouter02__factory.createInterface(),
-			functionName: 'getApprovalType',
-			functionParams,
-		})
+    const tx =
+      await this.multicall2Provider.callSameFunctionOnContractWithMultipleParams<
+        [string, string],
+        number
+      >({
+        address: SWAP_ROUTER_ADDRESS,
+        contractInterface: SwapRouter02__factory.createInterface(),
+        functionName: 'getApprovalType',
+        functionParams,
+      });
 
-		if (tx.results[0]?.success && tx.results[1]?.success) {
-			const resultTokenIn = tx.results![0]
-			const resultTokenOut = tx.results![1]
-			// TODO: Dealing with return values.
-			// return { approvalTokenIn: resultTokenIn?.result![0]!, approvalTokenOut: resultTokenOut?.result![0]!}
-		}
-		return { approvalTokenIn: 1, approvalTokenOut: 1}
+    if (!tx.results[0]?.success || !tx.results[1]?.success) {
+      log.info(
+        { results: tx.results },
+        'Failed to get approval type from swap router for token in or token out'
+      );
+      throw new Error(
+        'Failed to get approval type from swap router for token in or token out'
+      );
+    }
+
+    const { result: approvalTokenIn } = tx.results![0];
+    const { result: approvalTokenOut } = tx.results![1];
+
+    return { approvalTokenIn, approvalTokenOut };
   }
 }
