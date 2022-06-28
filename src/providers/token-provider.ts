fe0b58097c22cef1263ca18242e6200497270696
sawa <jesse.sawa@gmail.com>
2022-06-28 13:58:37 -0400

    fix: celo cusd & ceur decimal values

--- src/providers/token-provider.ts
+++ src/providers/token-provider.ts
@@ -405,7 +405,7 @@ export const DAI_CELO = new Token(
 export const CUSD_CELO = new Token(
   ChainId.CELO,
   '0x765DE816845861e75A25fCA122bb6898B8B1282a',
-  6,
+  18,
   'CUSD',
   'Celo Dollar Stablecoin'
 );
@@ -413,7 +413,7 @@ export const CUSD_CELO = new Token(
 export const CEUR_CELO = new Token(
   ChainId.CELO,
   '0xD8763CBa276a3738E6DE85b4b3bF5FDed6D6cA73',
-  6,
+  18,
   'CEUR',
   'Celo Euro Stablecoin'
 );
