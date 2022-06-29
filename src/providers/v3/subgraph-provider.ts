bfd2ed80ae5a6499345168418852926141a67f33
sawa <jesse.sawa@gmail.com>
2022-06-29 09:54:49 -0400

    fix: celo subgraph url

--- src/providers/v3/subgraph-provider.ts
+++ src/providers/v3/subgraph-provider.ts
@@ -55,9 +55,7 @@ const SUBGRAPH_URL_BY_CHAIN: { [chainId in ChainId]?: string } = {
     'https://api.thegraph.com/subgraphs/name/ianlapham/arbitrum-minimal',
   [ChainId.POLYGON]:
     'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-polygon',
-
-  //TODO replace subgraph with Celo Mainnet after deployment
-  [ChainId.CELO_ALFAJORES]: 'https://api.thegraph.com/subgraphs/name/joaquin-hernandez/uniswapv3-celo-alfajores-v1',
+  [ChainId.CELO]: 'https://api.thegraph.com/subgraphs/name/joaquin-hernandez/uniswapv3-subgraph-celo',
   [ChainId.GÖRLI]:
     'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-gorli',
 };
