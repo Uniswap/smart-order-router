1be3c959d162e481df3d2e99f3aa12555755a103
Raghava Pamula <raghava.pamula@uniswap.org>
2022-07-31 17:14:39 -0400

    Refactored

--- src/providers/util.ts
+++ src/providers/util.ts
@@ -44,70 +44,70 @@ export async function GetHighestLiquidityNativePool(
     return maxPool;
   }
   
-  export async function GetHighestLiquidityUSDPool(
-    chainId: ChainId,
-    poolProvider: IV3PoolProvider
-  ): Promise<Pool> {
-    const usdTokens = usdGasTokensByChain[chainId];
-    const wrappedCurrency = WRAPPED_NATIVE_CURRENCY[chainId]!;
-  
-    if (!usdTokens) {
-      throw new Error(
-        `Could not find a USD token for computing gas costs on ${chainId}`
-      );
-    }
-  
-    const usdPools = _([
-      FeeAmount.HIGH,
-      FeeAmount.MEDIUM,
-      FeeAmount.LOW,
-      FeeAmount.LOWEST,
-    ])
-      .flatMap((feeAmount) => {
-        return _.map<Token, [Token, Token, FeeAmount]>(
-          usdTokens,
-          (usdToken) => [wrappedCurrency, usdToken, feeAmount]
-        );
-      })
-      .value();
-  
-    const poolAccessor = await poolProvider.getPools(usdPools);
-  
-    const pools = _([
-      FeeAmount.HIGH,
-      FeeAmount.MEDIUM,
-      FeeAmount.LOW,
-      FeeAmount.LOWEST,
-    ])
-      .flatMap((feeAmount) => {
-        const pools = [];
-  
-        for (const usdToken of usdTokens) {
-          const pool = poolAccessor.getPool(
-            wrappedCurrency,
-            usdToken,
-            feeAmount
-          );
-          if (pool) {
-            pools.push(pool);
-          }
-        }
-  
-        return pools;
-      })
-      .compact()
-      .value();
-  
-    if (pools.length == 0) {
-      const message = `Could not find a USD/${wrappedCurrency.symbol} pool for computing gas costs.`;
-      log.error({ pools }, message);
-      throw new Error(message);
-    }
-  
-    const maxPool = _.maxBy(pools, (pool) => pool.liquidity) as Pool;
-  
-    return maxPool;
+export async function GetHighestLiquidityUSDPool(
+  chainId: ChainId,
+  poolProvider: IV3PoolProvider
+): Promise<Pool> {
+  const usdTokens = usdGasTokensByChain[chainId];
+  const wrappedCurrency = WRAPPED_NATIVE_CURRENCY[chainId]!;
+
+  if (!usdTokens) {
+    throw new Error(
+      `Could not find a USD token for computing gas costs on ${chainId}`
+    );
   }
+
+  const usdPools = _([
+    FeeAmount.HIGH,
+    FeeAmount.MEDIUM,
+    FeeAmount.LOW,
+    FeeAmount.LOWEST,
+  ])
+    .flatMap((feeAmount) => {
+      return _.map<Token, [Token, Token, FeeAmount]>(
+        usdTokens,
+        (usdToken) => [wrappedCurrency, usdToken, feeAmount]
+      );
+    })
+    .value();
+
+  const poolAccessor = await poolProvider.getPools(usdPools);
+
+  const pools = _([
+    FeeAmount.HIGH,
+    FeeAmount.MEDIUM,
+    FeeAmount.LOW,
+    FeeAmount.LOWEST,
+  ])
+    .flatMap((feeAmount) => {
+      const pools = [];
+
+      for (const usdToken of usdTokens) {
+        const pool = poolAccessor.getPool(
+          wrappedCurrency,
+          usdToken,
+          feeAmount
+        );
+        if (pool) {
+          pools.push(pool);
+        }
+      }
+
+      return pools;
+    })
+    .compact()
+    .value();
+
+  if (pools.length == 0) {
+    const message = `Could not find a USD/${wrappedCurrency.symbol} pool for computing gas costs.`;
+    log.error({ pools }, message);
+    throw new Error(message);
+  }
+
+  const maxPool = _.maxBy(pools, (pool) => pool.liquidity) as Pool;
+
+  return maxPool;
+}
 
 export async function getGasCostsInUSDandQuote(tokenIn: Token, l1FeeInWei: BigNumber, poolProvider: IV3PoolProvider) {
     // wrap fee to native currency
