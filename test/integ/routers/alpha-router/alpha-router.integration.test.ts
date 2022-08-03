a43e38dfbdc0f31b75673a062b72baf9532dc03c
Eric Zhong <eric.zhong@uniswap.org>
2022-08-02 21:39:31 -0700

    Unskip tests

--- test/integ/routers/alpha-router/alpha-router.integration.test.ts
+++ test/integ/routers/alpha-router/alpha-router.integration.test.ts
@@ -859,7 +859,7 @@ describe('alpha router integration', () => {
         });
       });
 
-      xit(`erc20 -> erc20 no recipient/deadline/slippage`, async () => {
+      it(`erc20 -> erc20 no recipient/deadline/slippage`, async () => {
         const tokenIn = USDC_MAINNET;
         const tokenOut = USDT_MAINNET;
         const amount =
@@ -884,7 +884,7 @@ describe('alpha router integration', () => {
         await validateSwapRoute(quote, quoteGasAdjusted, tradeType, 100, 10);
       });
 
-      xit(`erc20 -> erc20 gas price specified`, async () => {
+      it(`erc20 -> erc20 gas price specified`, async () => {
         const tokenIn = USDC_MAINNET;
         const tokenOut = USDT_MAINNET;
         const amount =
