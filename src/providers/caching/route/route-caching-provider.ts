ef8b8ecd8d8a3a2506a7b42c45c994056a8696ff
Miguel Cervera <miguel.cervera@uniswap.org>
2023-03-08 15:15:09 -0800

    Added documentation to the route caching provider

--- src/providers/caching/route/route-caching-provider.ts
+++ src/providers/caching/route/route-caching-provider.ts
@@ -12,7 +12,24 @@ import { ChainId } from '../../../util';
 import { CacheMode } from './model';
 import { CachedRoutes } from './model/cached-routes';
 
+/**
+ * Abstract class for a RouteCachingProvider.
+ * Defines the base methods of how to interact with this interface, but not the implementation of how to cache.
+ */
 export abstract class IRouteCachingProvider {
+  /**
+   * Final implementation of the public `getCachedRoute` method, this is how code will interact with the implementation
+   * This function is defined as a readonly member instead of a traditional function to make sure that it is final.
+   *
+   * @public
+   * @readonly
+   * @param chainId
+   * @param amount
+   * @param quoteToken
+   * @param tradeType
+   * @param protocols
+   * @param blockNumber
+   */
   public readonly getCachedRoute = (
     chainId: number,
     amount: CurrencyAmount<Currency>,
@@ -22,17 +39,19 @@ export abstract class IRouteCachingProvider {
     blockNumber: number,
   ): Promise<CachedRoutes | undefined> => {
     return this._getCachedRoute(chainId, amount, quoteToken, tradeType, protocols, blockNumber).then((cachedRoute) =>
-      Promise.resolve(this.validateCachedNotExpired(cachedRoute, blockNumber))
+      Promise.resolve(this.filterExpiredCachedRoutes(cachedRoute, blockNumber))
     );
   };
 
-  private validateCachedNotExpired(
-    cachedRoutes: CachedRoutes | undefined,
-    blockNumber: number
-  ): CachedRoutes | undefined {
-    return cachedRoutes?.notExpired(blockNumber) ? cachedRoutes : undefined;
-  }
-
+  /**
+   * Final implementation of the public `setCachedRoute` method.
+   * This method will set the blockToLive in the CachedRoutes object before calling the internal method to insert in cache.
+   *
+   * @public
+   * @readonly
+   * @param cachedRoutes The route to cache.
+   * @returns Promise<boolean> Indicates if the route was inserted into cache.
+   */
   public readonly setCachedRoute = (cachedRoutes: CachedRoutes): Promise<boolean> => {
     cachedRoutes.blocksToLive = this._getBlocksToLive(cachedRoutes);
 
@@ -40,10 +59,36 @@ export abstract class IRouteCachingProvider {
   };
 
   /**
-   * Abstract Methods
+   * Returns the CacheMode for the given combination of chainId, tokenIn, tokenOut and tradetype
+   *
+   * @public
+   * @abstract
+   * @param chainId
+   * @param tokenIn
+   * @param tokenOut
+   * @param tradeType
    */
   public abstract getCacheMode(chainId: ChainId, tokenIn: string, tokenOut: string, tradeType: TradeType): CacheMode
 
+  private filterExpiredCachedRoutes(
+    cachedRoutes: CachedRoutes | undefined,
+    blockNumber: number
+  ): CachedRoutes | undefined {
+    return cachedRoutes?.notExpired(blockNumber) ? cachedRoutes : undefined;
+  }
+
+  /**
+   * Internal function to fetch the CachedRoute from the cache.
+   * Must be implemented.
+   *
+   * @param chainId
+   * @param amount
+   * @param quoteToken
+   * @param tradeType
+   * @param protocols
+   * @param blockNumber
+   * @protected
+   */
   protected abstract _getCachedRoute(
     chainId: number,
     amount: CurrencyAmount<Currency>,
@@ -53,10 +98,22 @@ export abstract class IRouteCachingProvider {
     blockNumber: number
   ): Promise<CachedRoutes | undefined>
 
-
+  /**
+   * Internal function to insert the CachedRoute into cache.
+   * Must be implemented.
+   *
+   * @param cachedRoutes
+   * @protected
+   */
   protected abstract _setCachedRoute(cachedRoutes: CachedRoutes): Promise<boolean>
 
+  /**
+   * Internal function to getBlocksToLive for a given cachedRoute.
+   * This function is call before attempting to insert the route into cache.
+   * Must be implemented.
+   *
+   * @param cachedRoutes
+   * @protected
+   */
   protected abstract _getBlocksToLive(cachedRoutes: CachedRoutes): number
-
-
 }
