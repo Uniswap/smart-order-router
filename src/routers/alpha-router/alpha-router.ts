dae313a096108f0ba5bec1f7203ca778176919e6
Lynn Yu <lynn.yu@uniswap.org>
2023-06-06 21:33:50 -0400

    this works lol

--- src/routers/alpha-router/alpha-router.ts
+++ src/routers/alpha-router/alpha-router.ts
@@ -392,17 +392,17 @@ export class AlphaRouter
               maxTimeout: 1000,
             },
             {
-              multicallChunk: 50,
+              multicallChunk: 10,
               gasLimitPerCall: 1_200_000,
               quoteMinSuccessRate: 0.1,
             },
             {
               gasLimitOverride: 3_000_000,
-              multicallChunk: 20,
+              multicallChunk: 5,
             },
             {
               gasLimitOverride: 3_000_000,
-              multicallChunk: 20,
+              multicallChunk: 5,
             },
             {
               baseBlockOffset: -10,
