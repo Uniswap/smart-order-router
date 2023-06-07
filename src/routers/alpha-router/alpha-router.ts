76c8ac278823b32d7ecd27d72237e04f22599584
lynn <41491154+lynnshaoyu@users.noreply.github.com>
2023-06-06 21:42:16 -0400

    fix: reduce chunk size for optimism (#246)
    * reduce chunk size for optimism
    
    * this works lol
    
    * better

--- src/routers/alpha-router/alpha-router.ts
+++ src/routers/alpha-router/alpha-router.ts
@@ -392,17 +392,17 @@ export class AlphaRouter
               maxTimeout: 1000,
             },
             {
-              multicallChunk: 110,
+              multicallChunk: 30,
               gasLimitPerCall: 1_200_000,
               quoteMinSuccessRate: 0.1,
             },
             {
               gasLimitOverride: 3_000_000,
-              multicallChunk: 45,
+              multicallChunk: 15,
             },
             {
               gasLimitOverride: 3_000_000,
-              multicallChunk: 45,
+              multicallChunk: 15,
             },
             {
               baseBlockOffset: -10,
