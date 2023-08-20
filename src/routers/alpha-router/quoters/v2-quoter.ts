2e0258afbec386b8edf53724855cfa58d64c1bcd
Siyu Jiang <91580504+jsy1218@users.noreply.github.com>
2023-08-20 13:30:24 -0700

    Revert "Chore: measure per protocol version getRoutesThenQuotes and getQuotes latencies (#316)"
    This reverts commit b8413a9d110e4216687902da963f530e7c31da8a.

--- src/routers/alpha-router/quoters/v2-quoter.ts
+++ src/routers/alpha-router/quoters/v2-quoter.ts
@@ -28,7 +28,6 @@ export class V2Quoter extends BaseQuoter<V2Route> {
   protected v2PoolProvider: IV2PoolProvider;
   protected v2QuoteProvider: IV2QuoteProvider;
   protected v2GasModelFactory: IV2GasModelFactory;
-  protected override quoterVersion = 'V2';
 
   constructor(
     v2SubgraphProvider: IV2SubgraphProvider,
