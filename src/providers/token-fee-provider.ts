7d553ba167f7046d20073f1bacb9b517cbc3dfa5
Mark Toda <toda.mark@gmail.com>
2023-08-22 16:10:09 -0700

    feat: add fee detector

--- src/providers/token-fee-provider.ts
+++ src/providers/token-fee-provider.ts
@@ -1,74 +1,75 @@
 import { Token } from '@uniswap/sdk-core';
 import _ from 'lodash';
 
-import { ITokenValidator__factory } from '../types/other/factories/ITokenValidator__factory';
+import { TokenFeeDetector__factory } from '../types/other/factories/TokenFeeDetector__factory';
 import { ChainId, log, WRAPPED_NATIVE_CURRENCY } from '../util';
 
 import { ICache } from './cache';
 import { IMulticallProvider } from './multicall-provider';
 import { ProviderConfig } from './provider';
+import { DEFAULT_ALLOWLIST } from './token-validator-provider';
 
-export const DEFAULT_ALLOWLIST = new Set<string>([
-  // RYOSHI. Does not allow transfers between contracts so fails validation.
-  '0x777E2ae845272a2F540ebf6a3D03734A5a8f618e'.toLowerCase(),
-]);
-
-export enum TokenValidationResult {
-  UNKN = 0,
-  FOT = 1,
-  STF = 2,
+export type TokenFeeResult = {
+  buyFeeBps: number,
+  sellFeeBps: number,
 }
 
-export interface TokenValidationResults {
-  getValidationByToken(token: Token): TokenValidationResult | undefined;
+// on detector failure, assume no fee
+const DEFAULT_TOKEN_FEE_RESULT = {
+  buyFeeBps: 0,
+  sellFeeBps: 0,
+};
+
+export interface TokenFeeResults {
+  getFeesByToken(token: Token): TokenFeeResult | undefined;
 }
 
-const TOKEN_VALIDATOR_ADDRESS = '0xb5ee1690b7dcc7859771148d0889be838fe108e0';
+const FEE_DETECTOR_ADDRESS = '0x57eC54d113719dDE9A90E6bE807524a86560E89D';
 const AMOUNT_TO_FLASH_BORROW = '1000';
 const GAS_LIMIT_PER_VALIDATE = 1_000_000;
 
 /**
- * Provider for getting token data.
+ * Provider for getting token fee data.
  *
  * @export
- * @interface ITokenValidatorProvider
+ * @interface ITokenFeeProvider
  */
-export interface ITokenValidatorProvider {
+export interface ITokenFeeProvider {
   /**
    * Gets the token at each address. Any addresses that are not valid ERC-20 are ignored.
    *
-   * @param addresses The token addresses to get.
+   * @param tokens The token addresses to fetch fees for.
    * @param [providerConfig] The provider config.
    * @returns A token accessor with methods for accessing the tokens.
    */
   validateTokens(
     tokens: Token[],
     providerConfig?: ProviderConfig
-  ): Promise<TokenValidationResults>;
+  ): Promise<TokenFeeResults>;
 }
 
-export class TokenValidatorProvider implements ITokenValidatorProvider {
+export class TokenFeeProvider implements ITokenFeeProvider {
   private CACHE_KEY = (chainId: ChainId, address: string) =>
     `token-${chainId}-${address}`;
 
-  private BASES: string[];
+  private BASE_TOKEN: string;
 
   constructor(
     protected chainId: ChainId,
     protected multicall2Provider: IMulticallProvider,
-    private tokenValidationCache: ICache<TokenValidationResult>,
-    private tokenValidatorAddress = TOKEN_VALIDATOR_ADDRESS,
+    private tokenFeeCache: ICache<TokenFeeResult>,
+    private tokenValidatorAddress = FEE_DETECTOR_ADDRESS,
     private gasLimitPerCall = GAS_LIMIT_PER_VALIDATE,
     private amountToFlashBorrow = AMOUNT_TO_FLASH_BORROW,
     private allowList = DEFAULT_ALLOWLIST
   ) {
-    this.BASES = [WRAPPED_NATIVE_CURRENCY[this.chainId]!.address];
+    this.BASE_TOKEN = WRAPPED_NATIVE_CURRENCY[this.chainId]!.address;
   }
 
   public async validateTokens(
     tokens: Token[],
     providerConfig?: ProviderConfig
-  ): Promise<TokenValidationResults> {
+  ): Promise<TokenFeeResults> {
     const tokenAddressToToken = _.keyBy(tokens, 'address');
     const addressesRaw = _(tokens)
       .map((token) => token.address)
@@ -76,17 +77,17 @@ export class TokenValidatorProvider implements ITokenValidatorProvider {
       .value();
 
     const addresses: string[] = [];
-    const tokenToResult: { [tokenAddress: string]: TokenValidationResult } = {};
+    const tokenToResult: { [tokenAddress: string]: TokenFeeResult } = {};
 
     // Check if we have cached token validation results for any tokens.
     for (const address of addressesRaw) {
       if (
-        await this.tokenValidationCache.has(
+        await this.tokenFeeCache.has(
           this.CACHE_KEY(this.chainId, address)
         )
       ) {
         tokenToResult[address.toLowerCase()] =
-          (await this.tokenValidationCache.get(
+          (await this.tokenFeeCache.get(
             this.CACHE_KEY(this.chainId, address)
           ))!;
       } else {
@@ -95,24 +96,24 @@ export class TokenValidatorProvider implements ITokenValidatorProvider {
     }
 
     log.info(
-      `Got token validation results for ${
+      `Got token fee results for ${
         addressesRaw.length - addresses.length
       } tokens from cache. Getting ${addresses.length} on-chain.`
     );
 
     const functionParams = _(addresses)
-      .map((address) => [address, this.BASES, this.amountToFlashBorrow])
-      .value() as [string, string[], string][];
+      .map((address) => [address, this.BASE_TOKEN, this.amountToFlashBorrow])
+      .value() as [string, string, string][];
 
     // We use the validate function instead of batchValidate to avoid poison pill problem.
     // One token that consumes too much gas could cause the entire batch to fail.
     const multicallResult =
       await this.multicall2Provider.callSameFunctionOnContractWithMultipleParams<
-        [string, string[], string], // address, base token addresses, amount to borrow
-        [number]
+        [string, string, string], // address, base token address, amount to borrow
+        [TokenFeeResult]
       >({
         address: this.tokenValidatorAddress,
-        contractInterface: ITokenValidator__factory.createInterface(),
+        contractInterface: TokenFeeDetector__factory.createInterface(),
         functionName: 'validate',
         functionParams: functionParams,
         providerConfig,
@@ -127,9 +128,9 @@ export class TokenValidatorProvider implements ITokenValidatorProvider {
       const token = tokenAddressToToken[tokenAddress]!;
 
       if (this.allowList.has(token.address.toLowerCase())) {
-        tokenToResult[token.address.toLowerCase()] = TokenValidationResult.UNKN;
+        tokenToResult[token.address.toLowerCase()] = DEFAULT_TOKEN_FEE_RESULT;
 
-        await this.tokenValidationCache.set(
+        await this.tokenFeeCache.set(
           this.CACHE_KEY(this.chainId, token.address.toLowerCase()),
           tokenToResult[token.address.toLowerCase()]!
         );
@@ -151,16 +152,16 @@ export class TokenValidatorProvider implements ITokenValidatorProvider {
       const validationResult = resultWrapper.result[0]!;
 
       tokenToResult[token.address.toLowerCase()] =
-        validationResult as TokenValidationResult;
+        validationResult as TokenFeeResult;
 
-      await this.tokenValidationCache.set(
+      await this.tokenFeeCache.set(
         this.CACHE_KEY(this.chainId, token.address.toLowerCase()),
         tokenToResult[token.address.toLowerCase()]!
       );
     }
 
     return {
-      getValidationByToken: (token: Token) =>
+      getFeesByToken: (token: Token) =>
         tokenToResult[token.address.toLowerCase()],
     };
   }
