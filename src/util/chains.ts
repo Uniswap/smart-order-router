5b3cde76b287a37eb8492846d0db96336e2c57b9
Miguel Cervera <miguel.cervera@uniswap.org>
2024-02-22 11:05:19 -0800

    imports lint

--- src/util/chains.ts
+++ src/util/chains.ts
@@ -1,4 +1,10 @@
-import { ChainId, Currency, Ether, NativeCurrency, Token, } from '@uniswap/sdk-core';
+import {
+  ChainId,
+  Currency,
+  Ether,
+  NativeCurrency,
+  Token,
+} from '@uniswap/sdk-core';
 
 // WIP: Gnosis, Moonbeam
 export const SUPPORTED_CHAINS: ChainId[] = [
