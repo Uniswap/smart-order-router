1fb8f530316853a3ac0b02b6f27fe0f017681e1e
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-03-12 09:58:44 -0700

    feat: support blast (#511)
    * bump sdk versions
    
    * add blast new chain sor process
    
    * fix remaining compiling error
    
    * fix integ-test compiling error
    
    * fix missing gas deviation in integ-test
    
    * add blast integ-test
    
    * add blast into integ-test
    
    * add blast into github workflow
    
    * fix base token ordering and blast env var
    
    * fix import for USDB_BLAST
    
    * skip blast tenderly simulation integ-test
    
    * fix exact out low weth amount and github test for remaining setup
    
    * style
    
    * blast subgraph v3

--- src/routers/alpha-router/alpha-router.ts
+++ src/routers/alpha-router/alpha-router.ts
@@ -548,6 +548,7 @@ export class AlphaRouter
           );
           break;
         case ChainId.BASE:
+        case ChainId.BLAST:
         case ChainId.BASE_GOERLI:
           this.onChainQuoteProvider = new OnChainQuoteProvider(
             chainId,
