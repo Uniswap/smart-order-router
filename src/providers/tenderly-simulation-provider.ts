3b1cf186c4289697a1b2fb23401a5cfa5061a8d2
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-06-18 11:28:55 -0700

    fix: return tenderly not supported on zksync

--- src/providers/tenderly-simulation-provider.ts
+++ src/providers/tenderly-simulation-provider.ts
@@ -271,7 +271,7 @@ export class TenderlySimulator extends Simulator {
     const currencyIn = swapRoute.trade.inputAmount.currency;
     const tokenIn = currencyIn.wrapped;
     const chainId = this.chainId;
-    if ([ChainId.CELO, ChainId.CELO_ALFAJORES].includes(chainId)) {
+    if ([ChainId.CELO, ChainId.CELO_ALFAJORES, ChainId.ZKSYNC].includes(chainId)) {
       const msg = 'Celo not supported by Tenderly!';
       log.info(msg);
       return { ...swapRoute, simulationStatus: SimulationStatus.NotSupported };
