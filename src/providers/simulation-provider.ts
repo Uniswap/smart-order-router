b2e5f852081a1fe6c400bcd3351a2d82ab5b7527
Christos Gkolias <christos.gkolias@uniswap.org>
2024-07-26 22:27:37 +0300

    remove something

--- src/providers/simulation-provider.ts
+++ src/providers/simulation-provider.ts
@@ -64,13 +64,17 @@ export abstract class Simulator {
     quote: CurrencyAmount,
     providerConfig?: GasModelProviderConfig
   ): Promise<SwapRoute> {
+    const neededBalance =
+      swapRoute.trade.tradeType == TradeType.EXACT_INPUT ? amount : quote;
     if (
-      await this.userHasSufficientBalance(
+      // we assume we always have enough eth mainnet balance because we use beacon address later
+      (neededBalance.currency.isNative && this.chainId == ChainId.MAINNET) ||
+      (await this.userHasSufficientBalance(
         fromAddress,
         swapRoute.trade.tradeType,
         amount,
         quote
-      )
+      ))
     ) {
       log.info(
         'User has sufficient balance to simulate. Simulating transaction.'
