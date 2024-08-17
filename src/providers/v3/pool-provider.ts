85d1dc05cae0c0bea26f5201343b24b925afd7e9
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-08-16 20:23:24 -0700

    add v4 pool provider impl with refactor

--- src/providers/v3/pool-provider.ts
+++ src/providers/v3/pool-provider.ts
@@ -14,11 +14,6 @@ import { ProviderConfig } from '../provider';
 type ISlot0 = {
   sqrtPriceX96: BigNumber;
   tick: number;
-  observationIndex: number;
-  observationCardinality: number;
-  observationCardinalityNext: number;
-  feeProtocol: number;
-  unlocked: boolean;
 };
 
 type ILiquidity = { liquidity: BigNumber };
---
+++ src/providers/v4/pool-provider.ts
@@ -0,0 +1,158 @@
+import { ChainId, Currency } from '@uniswap/sdk-core';
+import { ProviderConfig } from '../provider';
+import { Pool } from '@uniswap/v4-sdk';
+import retry, { Options as RetryOptions } from 'async-retry';
+import { IMulticallProvider, Result } from '../multicall-provider';
+import {
+  log,
+  STATE_VIEW_ADDRESSES,
+} from '../../util';
+import { StateView__factory } from '../../types/other';
+import { ILiquidity, ISlot0, PoolProvider } from '../pool-provider';
+import { sortsBefore } from '@uniswap/v4-sdk/dist/utils/sortsBefore';
+import { ADDRESS_ZERO } from '@uniswap/router-sdk';
+
+type V4ISlot0 = ISlot0 & {
+  poolId: string;
+  protocolFee: number;
+  lpFee: number;
+};
+
+type V4ILiquidity = ILiquidity;
+
+export interface IV4PoolProvider {
+  getPools(
+    currencyPairs: [Currency, Currency, number, number, string][],
+    providerConfig?: ProviderConfig
+  ): Promise<V4PoolAccessor>;
+
+  getPoolId(
+    currencyA: Currency,
+    currencyB: Currency,
+    fee: number,
+    tickSpacing: number,
+    hooks: string
+  ): string;
+}
+
+export type V4PoolAccessor = {
+  getPool: (
+    currencyA: Currency,
+    currencyB: Currency,
+    fee: number,
+    tickSpacing: number,
+    hooks: string
+  ) => Pool | undefined;
+  getPoolById: (poolId: string) => Pool | undefined;
+  getAllPools: () => Pool[];
+};
+
+export type V4PoolRetryOptions = RetryOptions;
+export type V4PoolConstruct = [Currency, Currency, number, number, string];
+
+export class V4PoolProvider extends PoolProvider<Currency, V4PoolConstruct, V4ISlot0, V4ILiquidity, V4PoolAccessor> implements IV4PoolProvider {
+  // Computing pool id is slow as it requires hashing, encoding etc.
+  // Addresses never change so can always be cached.
+  private POOL_ID_CACHE: { [key: string]: string } = {};
+
+  /**
+   * Creates an instance of V4PoolProvider.
+   * @param chainId The chain id to use.
+   * @param multicall2Provider The multicall provider to use to get the pools.
+   * @param retryOptions The retry options for each call to the multicall.
+   */
+  constructor(
+    chainId: ChainId,
+    multicall2Provider: IMulticallProvider,
+    retryOptions: V4PoolRetryOptions = {
+      retries: 2,
+      minTimeout: 50,
+      maxTimeout: 500,
+    }
+  ) {
+    super(chainId, multicall2Provider, retryOptions);
+  }
+
+  public async getPools(
+    currencyPairs: V4PoolConstruct[],
+    providerConfig?: ProviderConfig
+  ): Promise<V4PoolAccessor> {
+    return await super.getPoolsInternal<V4PoolAccessor>(currencyPairs, providerConfig);
+  }
+
+  public getPoolId(currencyA: Currency, currencyB: Currency, fee: number, tickSpacing: number, hooks: string): string {
+    return Pool.getPoolId(currencyA, currencyB, fee, tickSpacing, hooks);
+  }
+
+  protected override async getPoolsData<TReturn>(
+    poolIds: string[],
+    functionName: string,
+    providerConfig?: ProviderConfig
+  ): Promise<Result<TReturn>[]> {
+    const { results, blockNumber } = await retry(async () => {
+      return this.multicall2Provider.callSameFunctionOnContractWithMultipleParams<
+        string[],
+        TReturn
+      >({
+        address: STATE_VIEW_ADDRESSES[this.chainId]!,
+        contractInterface: StateView__factory.createInterface(),
+        functionName: functionName,
+        functionParams: poolIds.map((poolId) => [poolId]),
+        providerConfig,
+      });
+    }, this.retryOptions);
+
+    log.debug(`Pool data fetched as of block ${blockNumber}`);
+
+    return results;
+  }
+
+  protected override getPoolIdentifier(pool: V4PoolConstruct): { poolIdentifier: string, currency0: Currency, currency1: Currency } {
+    const [currencyA, currencyB, fee, tickSpacing, hooks]  = pool;
+
+    const [currency0, currency1] = sortsBefore(currencyA, currencyB)
+      ? [currencyA, currencyB]
+      : [currencyB, currencyA];
+    const currency0Addr = currency0.isNative ? ADDRESS_ZERO : currency0.wrapped.address
+    const currency1Addr = currency1.isNative ? ADDRESS_ZERO : currency1.wrapped.address
+
+    const cacheKey = `${this.chainId}/${currency0Addr}/${currency1Addr}/${fee}/${tickSpacing}/${hooks}`;
+
+    const cachedId = this.POOL_ID_CACHE[cacheKey];
+
+    if (cachedId) {
+      return { poolIdentifier: cachedId, currency0, currency1 };
+    }
+
+    const poolId = Pool.getPoolId(currency0, currency1, fee, tickSpacing, hooks);
+
+    this.POOL_ID_CACHE[cacheKey] = poolId;
+
+    return { poolIdentifier: poolId, currency0, currency1 };
+  }
+
+  protected instantiatePool(pool: V4PoolConstruct, slot0: V4ISlot0, liquidity: V4ILiquidity): Pool {
+    const [currency0, currency1, fee, tickSpacing, hooks] = pool;
+
+    return new Pool(currency0, currency1, fee, tickSpacing, hooks, slot0.sqrtPriceX96.toString(), liquidity.liquidity.toString(), slot0.tick);
+  }
+
+  protected instantiatePoolAccessor(poolIdentifierToPool: {
+    [p: string]: Pool
+  }): V4PoolAccessor {
+    return {
+      getPool: (
+        currencyA: Currency,
+        currencyB: Currency,
+        fee: number,
+        tickSpacing: number,
+        hooks: string
+      ): Pool | undefined => {
+        const { poolIdentifier } = this.getPoolIdentifier([currencyA, currencyB, fee, tickSpacing, hooks]);
+        return poolIdentifierToPool[poolIdentifier];
+      },
+      getPoolById: (poolId: string): Pool | undefined => poolIdentifierToPool[poolId],
+      getAllPools: (): Pool[] => Object.values(poolIdentifierToPool),
+    };
+  }
+}
