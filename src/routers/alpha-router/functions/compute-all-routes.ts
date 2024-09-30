2f1226c23bafaed884288f9885ee945cc6ae9197
Siyu Jiang (See-You John) <91580504+jsy1218@users.noreply.github.com>
2024-09-30 08:54:24 -0700

    feat: part 1 native currency routing support by removing .wrapped in implementation classes (#723)

--- src/routers/alpha-router/functions/compute-all-routes.ts
+++ src/routers/alpha-router/functions/compute-all-routes.ts
@@ -21,7 +21,6 @@ export function computeAllV4Routes(
   pools: V4Pool[],
   maxHops: number
 ): V4Route[] {
-  // TODO: ROUTE-217 - Support native currency routing in V4
   return computeAllRoutes<V4Pool, V4Route, Currency>(
     currencyIn,
     currencyOut,
@@ -154,7 +153,6 @@ export function computeAllRoutes<
         ? curPool.token1
         : curPool.token0;
 
-      // TODO: ROUTE-217 - Support native currency routing in V4
       if (tokensVisited.has(getAddressLowerCase(currentTokenOut))) {
         continue;
       }
