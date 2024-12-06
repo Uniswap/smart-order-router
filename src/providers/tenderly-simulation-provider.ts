81583378fa935b37ba52167d081a757115307651
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-12-06 13:01:48 -0800

    feat: tenderly sim status SlippageTooLow

--- src/providers/tenderly-simulation-provider.ts
+++ src/providers/tenderly-simulation-provider.ts
@@ -464,6 +464,19 @@ export class TenderlySimulator extends Simulator {
               2
             )}.`
           );
+
+          if ((resp.result[2] as JsonRpcError).error.data) {
+            switch ((resp.result[2] as JsonRpcError).error.data) {
+              case '0x739dbe52': // V3TooMuchRequested
+              case '0x39d35496': // V3TooLittleReceived
+              case '0x849eaf98': // V2TooLittleReceived
+              case '0x8ab0bc16': // V2TooMuchRequested
+              case '0x08c379a000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000025556e697377617056323a20494e53554646494349454e545f4f55545055545f414d4f554e54000000000000000000000000000000000000000000000000000000': // INSUFFICIENT_OUTPUT_AMOUNT
+              case '0x08c379a0000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000034949410000000000000000000000000000000000000000000000000000000000': // IIA
+                return { ...swapRoute, simulationStatus: SimulationStatus.SlippageTooLow };
+            }
+          }
+
           return { ...swapRoute, simulationStatus: SimulationStatus.Failed };
         }
 
