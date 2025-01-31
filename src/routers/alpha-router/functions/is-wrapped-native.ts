37e4806c6496ac0adb93293b2da3a802762a1000
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2025-01-31 08:25:19 -0800

    remove is-wrapped-native

--- src/routers/alpha-router/functions/is-wrapped-native.ts
+++
@@ -1,6 +0,0 @@
-import { Currency } from '@uniswap/sdk-core';
-import { nativeOnChain } from '../../../util';
-
-export function isWrappedNative(currency: Currency): boolean {
-  return currency.wrapped.equals(nativeOnChain(currency.chainId).wrapped);
-}
