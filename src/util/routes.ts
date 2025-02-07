decea23119d645e99be241f95deaa32fedd69742
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2025-02-06 20:39:31 -0800

    routeString skip fake v4 pool

--- src/util/routes.ts
+++ src/util/routes.ts
@@ -1,5 +1,5 @@
 import { Protocol } from '@uniswap/router-sdk';
-import { Currency, Percent } from '@uniswap/sdk-core';
+import { ChainId, Currency, Percent } from '@uniswap/sdk-core';
 import { Pair } from '@uniswap/v2-sdk';
 import { Pool as V3Pool } from '@uniswap/v3-sdk';
 import { Pool as V4Pool } from '@uniswap/v4-sdk';
@@ -16,6 +16,7 @@ import { V3_CORE_FACTORY_ADDRESSES } from './addresses';
 import { TPool } from '@uniswap/router-sdk/dist/utils/TPool';
 import { CurrencyAmount } from '.';
 import { CachedRoutes } from '../providers';
+import { V4_ETH_WETH_FAKE_POOL } from './pool';
 
 export const routeToTokens = (route: SupportedRoutes): Currency[] => {
   switch (route.protocol) {
@@ -91,6 +92,12 @@ export const routeToString = (route: SupportedRoutes): string => {
         V3_CORE_FACTORY_ADDRESSES[pool.chainId]
       )}]`;
     } else if (pool instanceof V4Pool) {
+      // Special case in the case of ETH/WETH fake pool
+      // where we do not want to return the fake pool in the route string as it is not a real pool
+      if (pool.tickSpacing === V4_ETH_WETH_FAKE_POOL[pool.chainId as ChainId].tickSpacing) {
+        return ' ';
+      }
+
       return ` -- ${pool.fee / 10000}% [${V4Pool.getPoolId(
         pool.token0,
         pool.token1,
