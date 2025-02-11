4b9493a26b590bad087516431d4acec8ada2599a
Vasilis Xouris <vasileios.xouris@uniswap.org>
2025-02-11 09:15:14 -0800

    fix(tenderly): add unichain node endpoint

--- src/providers/tenderly-simulation-provider.ts
+++ src/providers/tenderly-simulation-provider.ts
@@ -152,6 +152,8 @@ const TENDERLY_NODE_API = (chainId: ChainId, tenderlyNodeApiKey: string) => {
       return `https://blast.gateway.tenderly.co/${tenderlyNodeApiKey}`;
     case ChainId.WORLDCHAIN:
       return `https://worldchain-mainnet.gateway.tenderly.co/${tenderlyNodeApiKey}`;
+    case ChainId.UNICHAIN:
+      return `https://unichain.gateway.tenderly.co/${tenderlyNodeApiKey}`;
     default:
       throw new Error(
         `ChainId ${chainId} does not correspond to a tenderly node endpoint`
