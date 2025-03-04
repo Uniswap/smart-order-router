63a3d5ced80875ceedeb524a7ef5d8d226a592b4
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2025-03-04 14:34:16 -0800

    fix remaining compiling error

--- src/util/pool.ts
+++ src/util/pool.ts
@@ -297,4 +297,14 @@ export const V4_ETH_WETH_FAKE_POOL: { [chainId in ChainId]: V4Pool } = {
     0,
     0
   ),
+  [ChainId.SONEIUM]: new V4Pool(
+    nativeOnChain(ChainId.SONEIUM),
+    nativeOnChain(ChainId.SONEIUM).wrapped,
+    0,
+    FAKE_TICK_SPACING,
+    ADDRESS_ZERO,
+    79228162514264337593543950336,
+    0,
+    0
+  )
 };
