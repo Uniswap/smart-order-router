e47ebbeb7957f76fb71454d764f197f02692d411
xrsv <vasileios.xouris@uniswap.org>
2025-05-01 00:38:53 +0300

    Revert "feat: skip cachedRoutes for large swaps" (#864)
    * Revert "feat: skip cachedRoutes for large swaps (#860)"
    
    This reverts commit 3be41b6d9756a033a748a6d99db888059eb50f29.
    
    * v4.21.4

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -18,7 +18,6 @@ export * from './simulation-provider';
 export * from './static-gas-price-provider';
 export * from './swap-router-provider';
 export * from './tenderly-simulation-provider';
-export * from './token-price-provider';
 export * from './token-properties-provider';
 export * from './token-provider';
 export * from './token-validator-provider';
