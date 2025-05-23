bf747821956234244318e65fb3fa415711ccfc1c
Yucan Liu <yucan.liu@uniswap.org>
2025-05-23 13:07:17 -0700

    formatting

--- src/routers/alpha-router/alpha-router.ts
+++ src/routers/alpha-router/alpha-router.ts
@@ -562,8 +562,8 @@ export type AlphaRouterConfig = {
 
 export class AlphaRouter
   implements
-  IRouter<AlphaRouterConfig>,
-  ISwapToRatio<AlphaRouterConfig, SwapAndAddConfig>
+    IRouter<AlphaRouterConfig>,
+    ISwapToRatio<AlphaRouterConfig, SwapAndAddConfig>
 {
   protected chainId: ChainId;
   protected provider: BaseProvider;
@@ -1421,8 +1421,8 @@ export class AlphaRouter
     // const gasTokenAccessor = await this.tokenProvider.getTokens([routingConfig.gasToken!]);
     const gasToken = routingConfig.gasToken
       ? (
-        await this.tokenProvider.getTokens([routingConfig.gasToken])
-      ).getTokenByAddress(routingConfig.gasToken)
+          await this.tokenProvider.getTokens([routingConfig.gasToken])
+        ).getTokenByAddress(routingConfig.gasToken)
       : undefined;
 
     const providerConfig: GasModelProviderConfig = {
@@ -3137,31 +3137,31 @@ export class AlphaRouter
     const nativeCurrency = WRAPPED_NATIVE_CURRENCY[this.chainId];
     const nativeAndQuoteTokenV3PoolPromise = !quoteToken.equals(nativeCurrency)
       ? getHighestLiquidityV3NativePool(
-        quoteToken,
-        this.v3PoolProvider,
-        providerConfig
-      )
+          quoteToken,
+          this.v3PoolProvider,
+          providerConfig
+        )
       : Promise.resolve(null);
     const nativeAndAmountTokenV3PoolPromise = !amountToken.equals(
       nativeCurrency
     )
       ? getHighestLiquidityV3NativePool(
-        amountToken,
-        this.v3PoolProvider,
-        providerConfig
-      )
+          amountToken,
+          this.v3PoolProvider,
+          providerConfig
+        )
       : Promise.resolve(null);
 
     // If a specific gas token is specified in the provider config
     // fetch the highest liq V3 pool with it and the native currency
     const nativeAndSpecifiedGasTokenV3PoolPromise =
       providerConfig?.gasToken &&
-        !providerConfig?.gasToken.equals(nativeCurrency)
+      !providerConfig?.gasToken.equals(nativeCurrency)
         ? getHighestLiquidityV3NativePool(
-          providerConfig?.gasToken,
-          this.v3PoolProvider,
-          providerConfig
-        )
+            providerConfig?.gasToken,
+            this.v3PoolProvider,
+            providerConfig
+          )
         : Promise.resolve(null);
 
     const [
@@ -3185,15 +3185,15 @@ export class AlphaRouter
 
     const v2GasModelPromise = this.v2Supported?.includes(this.chainId)
       ? this.v2GasModelFactory
-        .buildGasModel({
-          chainId: this.chainId,
-          gasPriceWei,
-          poolProvider: this.v2PoolProvider,
-          token: quoteToken,
-          l2GasDataProvider: this.l2GasDataProvider,
-          providerConfig: providerConfig,
-        })
-        .catch((_) => undefined) // If v2 model throws uncaught exception, we return undefined v2 gas model, so there's a chance v3 route can go through
+          .buildGasModel({
+            chainId: this.chainId,
+            gasPriceWei,
+            poolProvider: this.v2PoolProvider,
+            token: quoteToken,
+            l2GasDataProvider: this.l2GasDataProvider,
+            providerConfig: providerConfig,
+          })
+          .catch((_) => undefined) // If v2 model throws uncaught exception, we return undefined v2 gas model, so there's a chance v3 route can go through
       : Promise.resolve(undefined);
 
     const v3GasModelPromise = this.v3GasModelFactory.buildGasModel({
--- src/routers/alpha-router/quoters/v3-quoter.ts
+++ src/routers/alpha-router/quoters/v3-quoter.ts
@@ -99,7 +99,8 @@ export class V3Quoter extends BaseQuoter<V3CandidatePools, V3Route, Token> {
         // ROUTE-495 - bypass v3 pool FOT check for $ELMO token
         if (
           tokenValidation == TokenValidationResult.FOT &&
-          token.wrapped.address.toLowerCase() === '0x335f4e66b9b61cee5ceade4e727fcec20156b2f0' &&
+          token.wrapped.address.toLowerCase() ===
+            '0x335f4e66b9b61cee5ceade4e727fcec20156b2f0' &&
           (token.equals(tokenIn) || token.equals(tokenOut))
         ) {
           return false;
